3054596ad3e02b36b16f70acdc5aa05db46ae5aa
unknown <danovuksa3232@gmail.com>
2023-07-21 20:23:53 -0300

    cambios actual

--- server/index.js
+++ server/index.js
@@ -19,14 +19,10 @@ const router = require("./src/routes");
 const morgan = require("morgan");
 const cors = require("cors");
 const { conn } = require("./src/db.js");
+require("dotenv").config();
 
 const server = express();
 const httpServer = http.createServer(server);
-const io = socketIo(httpServer, {
-  cors: {
-    origin: "*",
-  },
-});
 
 // Configuración de Clerk
 // const clerk = new Clerk(process.env.CLERK_SECRET_KEY);
@@ -37,6 +33,49 @@ server.use(cors());
 
 server.use(router);
 
+//cors
+server.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    allowedHeaders: [
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'authorization',
+    ],
+  })
+  );
+
+  server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+  });
+
+  const io = socketIo(httpServer, {
+  pingTimeout: 10000,
+    cors: {
+      origin:"http://localhost:5173",
+    },
+  });
+
+
+  // server.use((req, res, next) => {
+    //   res.header("Access-Control-Allow-Origin", ""); // update to match the domain you will make the request from
+    //   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
+
 conn.sync({ force: true }).then(() => {
   console.log("Base de datos conectada");
   httpServer.listen(3001, () => {
@@ -45,8 +84,9 @@ conn.sync({ force: true }).then(() => {
 });
 
 // Configuración de Socket.io
-io.on("connection", (socket) => {
+io.on("connect", (socket) => {
   console.log("Se creo un usuario");
+
 
   socket.on("chat message", (data) => {
     io.emit("chat message", data);
