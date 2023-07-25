664e1978ba9cabec263afdcf724d49b2c61bbfbd
unknown <danovuksa3232@gmail.com>
2023-07-25 20:58:37 -0300

    cambios deploy

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,25 +1,37 @@
 const { Router } = require("express");
 const router = Router();
+const express = require("express");
+const path = require("path");
 
 //todos importacion de rutas
 //* rutas de usuarios
-const routerUser = require('./routerUser')
-const routerReportUser = require('./routerReportUser');
-const routerReviewUser = require('./routerReviewUser');
+const routerUser = require("./routerUser");
+const routerReportUser = require("./routerReportUser");
+const routerReviewUser = require("./routerReviewUser");
 const routerUserEvent = require("./routerUserEvent");
-const routerReportEvent = require('./routerReportEvent');
-const routerReviewEvent = require('./routerReviewEvent');
+//* rutas de eventos
+const routerEvents = require("./routerEvents");
+const routerReportEvent = require("./routerReportEvent");
+const routerReviewEvent = require("./routerReviewEvent");
+//* rutas de filtros
+const routerFilter = require("./routerFilter");
+//* rutas de admins
 const routerAdmins = require("./routerAdmins");
-const routerChatPersonal = require('./routerChatPersonal');
-const routerChatEvent = require('./routerChatEvent');
+//* rutas de chats
+const routerChatEvent = require("./routerChatEvent");
+const routerChatPersonal = require("./routerChatPersonal");
+//* rutas de emails
 const routerSendMail = require("./routerSendMail");
-const routerFilter = require('./routerFilter');
+
+
 
 //todos declaracion de rutas
 //* rutas de usuarios
 router.use("/reportuser", routerReportUser);
-router.use("/reviewuser", routerReviewUser);
 router.use("/users", routerUser);
+router.use("/reviewuser", routerReviewUser);
+//* rutas de eventos
+router.use("/events", routerEvents);
 router.use("/reportevent", routerReportEvent);
 router.use("/reviewevent", routerReviewEvent);
 router.use("/events", routerUserEvent);
@@ -27,8 +39,11 @@ router.use("/events", routerUserEvent);
 router.use("/filter", routerFilter);
 //* rutas de admins
 router.use("/", routerAdmins);
-router.use('/events', routerChatPersonal)
-router.use('/events', routerChatEvent)
+//* rutas de chats
+router.use("/events", routerChatEvent);
+router.use("/chat/personal", routerChatPersonal);
+//* rutas de emails
 router.use("/send-mail", routerSendMail);
+
 
 module.exports = router;
