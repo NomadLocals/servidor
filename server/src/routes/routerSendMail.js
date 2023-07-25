2ac99deb89ec66348efba5aafd2da36fda5c8ac3
JuanTosso <juantosso89@gmail.com>
2023-07-25 14:08:05 -0300

    arreglo rutas admin

--- server/src/routes/routerSendMail.js
+++ server/src/routes/routerSendMail.js
@@ -67,7 +67,7 @@ router.post("/register", (req, res) => {
 router.post("/newEventCreated", (req, res) => {
   try {
     const { email, userName, activityData } = req.body;
-    const {name, eventDate, description} = activityData
+    const { name, eventDate, description } = activityData;
     const contentHtml = `
     <p>¡Hola ${userName}!</p>
     <p>Has creado un evento exitosamente en Nomad Locals.</p>
@@ -96,6 +96,102 @@ router.post("/newEventCreated", (req, res) => {
           from: "Nomad Locals <nomad.locals01@gmail.com>",
           to: email,
           subject: "¡Has creado un encuentro exitosamente en Nomad Locals!",
+          html: contentHtml,
+        };
+        const result = transporter.sendMail(mailOptions);
+        return result;
+      } catch (error) {
+        res.status(400).send(error.message);
+      }
+    }
+    sendMail()
+      .then((result) => res.status(200).send("enviado"))
+      .catch((error) => console.log(error.message));
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+router.post("/delete", (req, res) => {
+  try {
+    const { email } = req.body;
+    const contentHtml = `
+    <p> Querido ${email},</p>
+    <p>Lamentamos informarte que tu cuenta en nuestro sitio web ha sido suspendida debido a incumplimiento de nuestras normas y políticas. Como resultado, se te ha denegado el acceso a tu cuenta y a las funcionalidades del sitio.</p>
+    <p>Si consideras que ha habido un error o que la suspensión es injusta, por favor, ponte en contacto con nuestro equipo de soporte para revisar tu caso y proporcionar más detalles. Puedes enviar un correo electrónico a nomad.locals01@gmail.com.</p>
+    <p>Entendemos que esto pueda ser inconveniente para ti y te pedimos disculpas por cualquier molestia causada. Nuestro objetivo es mantener una comunidad segura y respetuosa para todos nuestros usuarios, y tomamos las medidas necesarias para garantizar que se cumplan nuestras políticas.</p>
+    <br/>
+    <p>Gracias por tu comprensión.</p>
+    
+    <p>Equipo de Nomad Locals</p>
+    `;
+
+    async function sendMail() {
+      try {
+        const accessToken = await oAuth2Client.getAccessToken();
+        const transporter = nodemailer.createTransport({
+          service: "gmail",
+          auth: {
+            type: "OAuth2",
+            user: "nomad.locals01@gmail.com",
+            clientId: CLIENT_ID,
+            clientSecret: CLIENT_SECRET,
+            refreshToken: REFRESH_TOKEN,
+            accessToken,
+          },
+        });
+        const mailOptions = {
+          from: "Nomad Locals <nomad.locals01@gmail.com>",
+          to: email,
+          subject: "Nomad Locals: Cuenta suspendida - Acceso restringido",
+          html: contentHtml,
+        };
+        const result = transporter.sendMail(mailOptions);
+        return result;
+      } catch (error) {
+        res.status(400).send(error.message);
+      }
+    }
+    sendMail()
+      .then((result) => res.status(200).send("enviado"))
+      .catch((error) => console.log(error.message));
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+router.post("/retrieve", (req, res) => {
+  try {
+    const { email } = req.body;
+    const contentHtml = `
+    <p> Estimado/a ${email},</p>
+    <p>Nos complace informarte que hemos revisado tu caso y hemos decidido reactivar tu cuenta en nuestro sitio web. Tu cuenta ha sido desbaneada y ahora tendrás acceso completo a todas las funcionalidades de nuestro sitio.</p>
+    <p>Entendemos que los errores pueden ocurrir, y después de una revisión cuidadosa, hemos decidido darte una segunda oportunidad para ser parte de nuestra comunidad en línea.</p>
+    <p>Si tienes alguna pregunta o inquietud, no dudes en ponerte en contacto con nuestro equipo de soporte en nomad.locals01@gmail.com o utilizando el formulario de contacto en nuestro sitio web.</p>
+    <p>¡Te damos la bienvenida de nuevo y esperamos que disfrutes de tu experiencia en nuestro sitio!.</p>
+    <br/>
+    <p>Gracias por ser parte de nuestra comunidad</p>
+    
+    <p>Equipo de Nomad Locals</p>
+    `;
+
+    async function sendMail() {
+      try {
+        const accessToken = await oAuth2Client.getAccessToken();
+        const transporter = nodemailer.createTransport({
+          service: "gmail",
+          auth: {
+            type: "OAuth2",
+            user: "nomad.locals01@gmail.com",
+            clientId: CLIENT_ID,
+            clientSecret: CLIENT_SECRET,
+            refreshToken: REFRESH_TOKEN,
+            accessToken,
+          },
+        });
+        const mailOptions = {
+          from: "Nomad Locals <nomad.locals01@gmail.com>",
+          to: email,
+          subject:
+            "Nomad Locals:  Reactivación de cuenta - Bienvenido de nuevo",
           html: contentHtml,
         };
         const result = transporter.sendMail(mailOptions);
