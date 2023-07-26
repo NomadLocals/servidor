7ac97a4a0cffe5faed6deacee5361e4f56330327
ryuksan <clinicasystemlab@gmail.com>
2023-07-26 15:56:21 -0500

    creacion de la documentacion de la api

--- server/src/server.js
+++ server/src/server.js
@@ -11,8 +11,6 @@ server.use(morgan("dev"));
 server.use(express.json());
 
   server.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    //  update to match the domain you will make the request from
     res.header(
       "Access-Control-Allow-Origin",
       "*"
