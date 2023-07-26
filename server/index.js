c46a7ae25e7290abca4a9bc3dff0aeaef1d2339c
unknown <danovuksa3232@gmail.com>
2023-07-26 20:34:08 -0300

    cambios deploy

--- server/index.js
+++ server/index.js
@@ -131,7 +131,7 @@ io.on("connection", (socket) => {
   
   
   
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   console.log("Base de datos conectada");
   // io.listen(3001, () => {
   //   console.log(`Servidor iniciado en ${PORT}`);
