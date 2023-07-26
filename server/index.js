aea59b8b99726fba8abd5cf3ce9adaaec5c7ecb1
ryuksan <clinicasystemlab@gmail.com>
2023-07-26 18:46:06 -0500

    ..

--- server/index.js
+++ server/index.js
@@ -1,18 +1,8 @@
-const { conn, ChatPersonal, Users } = require("./src/db.js");
+const { conn,  Users } = require("./src/db.js");
 require("dotenv").config();
 const {Server} = require('socket.io');
-const http = require('http');
-const {Op} = require('sequelize')
-
 const server = require('./src/server.js');
 const PORT = 3001
-// importaciones para la documentacion de la api
-const path = require("path");
-const marked = require('marked-gfm-heading-id');
-const { parse } = require('marked');
-// importaciones para la documentacion de la api
-
-// const app = http.createServer(server);
 const app = server.listen(PORT, () => {
   console.log(`Servidor iniciado en ${PORT}`);
 });
@@ -21,6 +11,10 @@ const io  = new Server(app, {
     origin: '*',
   }
 })
+// importaciones para la documentacion de la api
+const path = require("path");
+const marked = require('marked-gfm-heading-id');
+const { parse } = require('marked');
 // para la documentacion de la api
 const fs = require('fs');
 
@@ -35,43 +29,41 @@ server.get('/', (req, res) => {
     
     const htmlContent = parse(data);
     res.send(`
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <title>Documentación API</title>
-          <style>
-      body {
-        background-color: black;
-        color: #CCCCCC;
-        font-family: 'Arial', sans-serif;
-        font-size: 18px;
-        text-align: justify;
-      }
-      h1, h2, h3, h4, h5, h6 {
-        text-align: center;
-      }
-      img {
-        position: relative;
-        left: 20%;
-      }
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <title>Documentación API</title>
+    <style>
+    body {
+      background-color: black;
+      color: #CCCCCC;
+      font-family: 'Arial', sans-serif;
+      font-size: 18px;
+      text-align: justify;
+    }
+    h1, h2, h3, h4, h5, h6 {
+      text-align: center;
+    }
+    img {
+      position: relative;
+      left: 20%;
+    }
     </style>
-        </head>
-        <body>
-          ${htmlContent}
-        </body>
-      </html>
+    </head>
+    <body>
+    ${htmlContent}
+    </body>
+    </html>
     `);
   });
 });
-// para la documentacion de la api
+// importaciones para la documentacion de la api
 
 // chat socket.io
 const {createEventChat, getEventChatsByEvent } = require ('./src/controllers/controllerChatEvent.js')
-// const { createPersonalChat, getPersonalChatsByUsers } = require ('./src/controllers/controllerChatPersonal.js')
-
+const { createPersonalChat, getPersonalChatsByUsers } = require ('./src/controllers/controllerChatPersonal.js')
 
 io.on("connection", (socket) => {
-
 
   console.log(`client connected: ${socket.id}`);
 
@@ -82,44 +74,19 @@ io.on("connection", (socket) => {
   socket.on("joinPersonalChat", (roomName) => {
     socket.join(roomName);
   });
-  
-
-  // socket.on("chatPersonalMessage", async ({ roomName, senderId, receiverId, senderUserName, message }) => {
-
-  //   try {
-      
-  //       const chat = await createPersonalChat({senderId, receiverId, message, senderUserName });
-  //       // console.log(chat)
-      
-  //     // Emitir el evento "chatPersonalMessage" con el nuevo mensaje
-  //     const messageData = {
-  //       senderId: senderId,
-  //       senderUserName: senderUserName,
-  //       message: message,
-  //     };
-  //     // const roomName = [senderId, receiverId].sort().join("-");
-  //     // console.log(roomName)
-  //     io.to(roomName).emit("chatPersonalMessage", messageData);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // });
 
   socket.on('getPersonalMessage', async (roomName) => {
-    console.log(roomName)
     const allMessages = await getPersonalChatsByUsers(roomName);
     const historial = {
       usuario: allMessages.userName,
       message: allMessages.message,
     }
     socket.emit('getPersonalMessage', historial)
-    // socket.broadcast.emit('getPersonalMessage', allMessages)
   })
 
   socket.on("chatPersonalMessage", async ({senderId, receiverId, senderUserName, message}) => {
     const newPersonalChat = await createPersonalChat({senderId, receiverId, senderUserName, message})
     const user = await Users.findByPk(senderId)
-    // const message = `${message}`;
     const messageData = {
       senderId: senderId,
       senderUsername: senderUserName,
@@ -127,16 +94,14 @@ io.on("connection", (socket) => {
       receiverId: receiverId
     };
     const roomName = [senderId, receiverId].sort().join("-");
-    console.log(roomName)
     io.to(roomName).emit("chatPersonalMessage", messageData);
   });
-
 
   socket.on("chatEventMessage", async ({ userName,eventId, senderId, message }) => {
     const newEventChat = await createEventChat({userName ,eventId, senderId, message });
     const user = await Users.findByPk(senderId);
     const dataToSend = {
-    usuario: user.userName, // Asegúrate de que userName sea la propiedad correcta en tu modelo de usuario
+    usuario: user.userName, 
     message: newEventChat.message,
     userName: user.userName,
     senderId: user.id
@@ -144,7 +109,7 @@ io.on("connection", (socket) => {
     socket.emit("chatEventMessage", dataToSend);
     socket.broadcast.emit("chatEventMessage", dataToSend);
   });
-  
+
   socket.on('getMessagesEvent', async (eventId) => {
     const allMessages = await getEventChatsByEvent(eventId)
     const historial = {
@@ -152,16 +117,9 @@ io.on("connection", (socket) => {
       message : allMessages.message
     }  
     socket.emit('getMessagesEvent', historial);
-    // socket.broadcast.emit("getMessagesEvent", allMessages)
   })
 });
-  
-  
-  
-  
-conn.sync({ force: false }).then(() => {
+
+conn.sync({ force: true }).then(() => {
   console.log("Base de datos conectada");
-  // io.listen(3001, () => {
-  //   console.log(`Servidor iniciado en ${PORT}`);
-  // });
 });
