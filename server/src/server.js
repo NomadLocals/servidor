c132cf291c771c449dd78ea7a1190c83927114a0
Nomad Locals <138519528+NomadLocals@users.noreply.github.com>
2023-08-03 14:03:00 -0500

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -13,7 +13,7 @@ server.use(express.json());
   server.use((req, res, next) => {
     res.header(
       "Access-Control-Allow-Origin",
-      "https://nomandlocals.netlify.app"
+      "*"
       );
       
       res.header(
